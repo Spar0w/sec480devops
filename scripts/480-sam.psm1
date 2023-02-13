20a869d42e84f8a07ca9f169f30f4b8e348e2470
Sparrow <43831657+Spar0w@users.noreply.github.com>
2023-02-13 14:30:49 -0500

    finished module

--- scripts/480-sam.psm1
+++ scripts/480-sam.psm1
@@ -1,4 +1,4 @@
-#file with variables to reuse
+#Samuel Barrows 480-module
 
 function read-config($configfile){
     
@@ -25,65 +25,117 @@ function read-config($configfile){
 
 function connect-vcenter($vcenter){
     #try to connect to vcenter with our hash table options
-    try {
-        Connect-VIServer -Server $vcenter
-        #linkedcloner $vmtoclone $newname
-    } catch {
-        Write-Host "An error occurred connecting to Vcenter:"
-        Write-Host $_
-        exit
+    if (!$global:DefaultVIServers.Count -gt 0){
+        try {
+            Connect-VIServer -Server $vcenter
+        } catch {
+            Write-Host "An error occurred connecting to Vcenter:"
+            Write-Host $_
+            exit
+        }
     }
 
 }
 
 function get-vms($folder){
-    Get-Folder $folder
+    $vmsInFolder = @()
+    Get-Folder $folder | Get-VM | Select-Object -Property Name | `
+    ForEach-Object{
+        #add the names of each vm to an array
+        $vmsInFolder += $_
+    }
+    return $vmsInFolder
 }
 
-function cloner([hashtable]$vars){
+function select-vm($folder){
+    #Provide user input to select an available VM
+
+    #Get our list of VMs
+    $vmList = get-vms $folder
+
+    $vmtoclone = $null
+    while ($True){
+        #List base vms
+        $x=0
+        $vmList | ForEach-Object{
+            $name = $_.Name
+            Write-Host "[$x] $name"
+            $x+=1
+        }
+        $vmnum = Read-Host "Enter the number associated with the VM you would like to operate on"
+
+        #validate input
+        try {
+            if ([int]$vmnum -gt $vmList.Length - 1 || [int]$vmnum -lt 1){
+                Write-Error "Invalid option, please try again"
+            } else {
+                $vmtoclone = $vmList[[int]$vmnum].Name
+                break
+            }
+        } catch {
+            Write-Error "Invalid option, please try again"
+        }
+    }
+
+    if([string]::IsNullOrWhiteSpace($vmtoclone)) {
+        Write-Error "An Error Occured: VM not found"
+        exit
+    }
+    return $vmtoclone
+}
+
+function cloner($vars){
     #creates a full clone from a base snapshot
 
-    #read input from the user
-    get-vms "BASEVM"
-    $vmtoclone = Read-Host "Enter the number associated with the VM you would like to clone: "
+    $vmtoclone = select-vm "basevm"
+
+    $newname = Read-Host "Enter the name of the new clone"
+
+    Write-Host "Cloning $vmtoclone to $newname"
 
     try {
     	$vm=get-vm -name $vmtoclone
         $snapshot=get-snapshot -VM $vm -name "base"
-        $vmhost=get-vmhost -name "192.168.7.19"
-        $ds=Get-Datastore -name datastore2-super9
+        $vmhost=get-vmhost -name $vars["esxiIP"]
+        $ds=Get-Datastore -name $vars["datastore"]
 
         $linkedName = "{0}.linked" -f $vm.name
         $linkedVM = New-VM -LinkedClone -Name $linkedName -VM $vm -ReferenceSnapshot $snapshot -VMHost $vmhost -Datastore $ds
 
-    	$newVM = New-VM -name "$newname.base" -VM $linkedvm -VMHost $vmhost -Datastore $ds
+    	$newVM = New-VM -name "$newname" -VM $linkedvm -VMHost $vmhost -Datastore $ds
     	$newVM | New-Snapshot -Name "Base"
 
-	Remove-VM $linkedName
+	    Remove-VM $linkedName
     } catch {
         Write-Host "An error occurred:"
-        Write-Host $_
+        Write-Error $_
         return
     }
 }
 
-function linkedcloner($vmtoclone, $newname){
+function linkedcloner($vars){
     #creates a linked clone from a base snapshot
+    $vmtoclone = select-vm("basevm")
+
+    $newname = Read-Host "Enter the name of the new linked clone"
+
+    Write-Host "Cloning $vmtoclone to the linked clone $newname"
+
     try {
     	$vm=get-vm -name $vmtoclone
         $snapshot=get-snapshot -VM $vm -name "base"
-        $vmhost=get-vmhost -name $linkedvars["esxiIP"]
-        $ds=Get-Datastore -name $linkedvars["datastore"]
+        $vmhost=get-vmhost -name $vars["esxiIP"]
+        $ds=Get-Datastore -name $vars["datastore"]
 
         $linkedName = $newname 
         $linkedVM = New-VM -LinkedClone -Name $linkedName -VM $vm -ReferenceSnapshot $snapshot -VMHost $vmhost -Datastore $ds
 
         #set network adapter
-        $linkedVM | Get-NetworkAdapter | Set-NetworkAdapter -NetworkName $linkedvars["newnetadap"]
+        $linkedVM | Get-NetworkAdapter | Set-NetworkAdapter -NetworkName $vars["newnetadap"]
 
     } catch {
         Write-Host "An error occurred creating the linked clone:"
-        Write-Host $_
+        Write-Error $_
         exit
     }
 }
